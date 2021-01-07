78bf3ba82526144260ef19216d860fa4f3937dbc
nagmagit <rossogiuliano@gmail.com>
2021-01-07 04:17:42 -0300

    Added a TextView to display if the board is connected or not

--- app/src/main/java/com/nagma/suitelectronica/MainActivity.java
+++ app/src/main/java/com/nagma/suitelectronica/MainActivity.java
@@ -2,19 +2,17 @@ package com.nagma.suitelectronica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     Arduino arduino;
+    Timer detectionTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +20,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         arduino = new Arduino();
+        detectionTimer = new Timer();
+
+        startDetection();
     }
 
     public void openButton_Click(View view) {
 
+    }
+
+    private void startDetection() {
+        detectionTimer.scheduleAtFixedRate(
+                new TimerTask() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView detectedTextView = findViewById(R.id.detectedTextView);
+
+                                if (arduino.isConnected(getApplicationContext())) {
+                                    detectedTextView.setText(R.string.board_detected);
+                                } else {
+                                    detectedTextView.setText(R.string.board_notdetected);
+                                }
+                            }
+                        });
+                    }
+                }, 0, 500);
     }
 }
