b81d7f8425dd87d0e0535b79841a1b6067fc1396
nagmagit <rossogiuliano@gmail.com>
2021-01-10 01:00:19 -0300

    Finished serial communication prototype

--- app/src/main/java/com/nagma/suitelectronica/Arduino.java
+++ app/src/main/java/com/nagma/suitelectronica/Arduino.java
@@ -1,18 +1,108 @@
 package com.nagma.suitelectronica;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+import android.os.Handler;
 
 import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
 
 import java.util.ArrayList;
 import java.util.Map;
 
 public class Arduino {
 
-    public static final int ARDUINO_VID = 0x2341;
+    private static final int ARDUINO_VID = 0x2341;
+
+    public static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+
+    public UsbDevice usbDevice;
+    public UsbDeviceConnection usbConnection;
+    public UsbSerialDevice serialDevice;
+
+    private Context context;
+    private Handler readHandler;
+    private UsbManager manager;
+
+    /*
+     *  Data received from serial port will be received here.
+     */
+    private UsbSerialInterface.UsbReadCallback readCallback = data -> {
+        String message = data.toString();
+
+        if (readHandler != null)
+            readHandler.obtainMessage(0, message).sendToTarget();
+    };
+
+    public Arduino(Context context, Handler readHandler) {
+        manager = (UsbManager)context.getSystemService(Context.USB_SERVICE);
+        this.context = context;
+        this.readHandler = readHandler;
+    }
+
+    /*
+     * Initializes the serial device. Returns true if it's successful.
+     */
+    public boolean initialize(UsbDevice device) {
+        if (!manager.hasPermission(device)) {
+            throw new SecurityException("This context doesn't have permission for this device.");
+        }
+
+        usbDevice = device;
+        usbConnection = manager.openDevice(device);
+
+        serialDevice = UsbSerialDevice.createUsbSerialDevice(device, usbConnection);
+
+        return serialDevice != null;
+    }
+
+    /*
+     * Opens the connection to the serial device.
+     */
+    public void open(int baudrate) {
+        serialDevice.open();
+
+        configureSerialDevice(serialDevice, baudrate);
+
+        serialDevice.read(readCallback);
+    }
+
+    /*
+     * Closes the connection to the serial device.
+     */
+    public void close() {
+        serialDevice.close();
+    }
+
+    /*
+     * Writes data to the serial device.
+     */
+    public void write(byte[] data) {
+        if (serialDevice != null)
+            serialDevice.write(data);
+    }
+
+    private void configureSerialDevice(UsbSerialDevice serialDevice, int baudrate) {
+        serialDevice.setBaudRate(baudrate);
+        serialDevice.setDataBits(UsbSerialInterface.DATA_BITS_8);
+        serialDevice.setStopBits(UsbSerialInterface.STOP_BITS_1);
+        serialDevice.setParity(UsbSerialInterface.PARITY_NONE);
+
+        serialDevice.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+    }
+
+    /*
+     * Request user permission. The response will be received by whomever provided the context.
+     */
+    public void requestUserPermission(UsbDevice device) {
+        PendingIntent intent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+
+        manager.requestPermission(device, intent);
+    }
 
     /*
      * Returns all UsbDevices compatible with UsbSerial and that are Arduinos.
--- app/src/main/java/com/nagma/suitelectronica/MainActivity.java
+++ app/src/main/java/com/nagma/suitelectronica/MainActivity.java
@@ -2,12 +2,23 @@ package com.nagma.suitelectronica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -19,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
     private Timer detectionTimer;
 
     private Context context;
+    private UsbDevice detectedDevice;
+
+    public BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Arduino.ACTION_USB_PERMISSION)) {
+                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+
+                onPermissionResponse(granted);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +49,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         context = getApplicationContext();
+        setFilter();
 
         detectionTimer = new Timer();
         startDetection();
     }
 
-    public void openButton_Click(View view) {
+    private void onPermissionResponse(boolean granted) {
+        if (granted) {
+            Spinner baudrateSpinner = findViewById(R.id.baudrateSpinner);
+            int baudrate = Integer.parseInt((String)baudrateSpinner.getSelectedItem());
 
+            boolean initSuccess = arduino.initialize(detectedDevice);
+
+            if (initSuccess) {
+                arduino.open(baudrate);
+            }
+
+            toggleOpenState(initSuccess);
+        } else {
+            Toast.makeText(context, "Permission denied.", Toast.LENGTH_SHORT).show();
+        }
     }
 
+    private void onReceivedMessage(String message) {
+        EditText receivedEditText = findViewById(R.id.receivedEditText);
+        receivedEditText.append(message);
+    }
+
+    public void openButton_Click(View view) {
+        if (detectedDevice != null) {
+            arduino = new Arduino(context, new ReadHandler(this));
+            arduino.requestUserPermission(detectedDevice);
+        } else {
+            Toast.makeText(context, "No devices detected.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void closeButton_Click(View view) {
+        if (detectedDevice != null) {
+            arduino.close();
+            toggleOpenState(false);
+        } else {
+            Toast.makeText(context, "No devices detected.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void sendButton_Click(View view) {
+        EditText sendEditText = findViewById(R.id.sendEditText);
+        String messageToSend = sendEditText.getText().toString();
+
+        if (detectedDevice != null) {
+            arduino.write(messageToSend.getBytes());
+        } else {
+            Toast.makeText(context, "No devices detected.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+     * Enables or disables the views that should only be enabled when the connection is open.
+     */
+    private void toggleOpenState(boolean isOpen) {
+        findViewById(R.id.baudrateSpinner).setEnabled(!isOpen);
+        findViewById(R.id.openButton).setEnabled(!isOpen);
+
+        findViewById(R.id.closeButton).setEnabled(isOpen);
+        findViewById(R.id.sendEditText).setEnabled(isOpen);
+        findViewById(R.id.sendButton).setEnabled(isOpen);
+    }
+
+    /*
+     * Sets up the broadcast receiver to know when the user accepted the permission.
+     */
+    private void setFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Arduino.ACTION_USB_PERMISSION);
+        registerReceiver(receiver, filter);
+    }
+
+    /*
+     * Start a timer to check if there are any supported devices connected.
+     */
     private void startDetection() {
         detectionTimer.scheduleAtFixedRate(
                 new TimerTask() {
@@ -42,14 +137,33 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         runOnUiThread(() -> {
                             TextView detectedTextView = findViewById(R.id.detectedTextView);
+                            ArrayList<UsbDevice> supportedDevices = Arduino.getSupportedDevices(context);
 
-                            if (!Arduino.getSupportedDevices(context).isEmpty()) {
-                                detectedTextView.setText(R.string.board_detected);
-                            } else {
+                            if (supportedDevices.isEmpty()) {
                                 detectedTextView.setText(R.string.board_notdetected);
+                                detectedDevice = null;
+                                toggleOpenState(false);
+                            } else {
+                                detectedTextView.setText(R.string.board_detected);
+                                detectedDevice = supportedDevices.get(0);
                             }
                         });
                     }
                 }, 0, DETECTION_PERIOD);
+    }
+
+    private static class ReadHandler extends Handler {
+        private final WeakReference<MainActivity> activity;
+
+        public ReadHandler(MainActivity activity) {
+            this.activity = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message message) {
+            String data = (String) message.obj;
+
+            activity.get().onReceivedMessage(data);
+        }
     }
 }
